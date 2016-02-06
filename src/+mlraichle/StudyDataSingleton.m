9efc586c3b37cbd399a771cd72c793401d1cdecf
John Lee <jjlee.wustl.edu@gmail.com>
2016-02-05 22:34:45 -0600

    Checking-in progress on alignment/registration tools.

--- src/+mlraichle/StudyDataSingleton.m
+++ src/+mlraichle/StudyDataSingleton.m
@@ -9,22 +9,17 @@ classdef StudyDataSingleton < mlpipeline.StudyDataSingleton
  	%% It was developed on Matlab 9.0.0.307022 (R2016a) Prerelease for MACI64.
  	
 
-    properties (SetAccess = private)
-        raichleTrunk = '/Volumes/SeagateBP4/raichle'
-
-        mriFolder = ''
-        petFolder = ''
-        hdrinfoFolder = ''
-        fslFolder = 'fsl'
+    properties (SetAccess = protected)
+        raichleTrunk = '/Volumes/SeagateBP4/raichle/PPGdata'
     end
     
 	properties (Dependent)
-        subjectsDirs
+        subjectsDir
         loggingPath
     end
     
     methods %% GET
-        function g = get.subjectsDirs(this)
+        function g = get.subjectsDir(this)
             g = fullfile(this.raichleTrunk, 'idaif', '');
         end
         function g = get.loggingPath(this)
@@ -57,11 +52,68 @@ classdef StudyDataSingleton < mlpipeline.StudyDataSingleton
         end
     end
     
-	methods (Access = private)	   
+    methods
+        function f = fslFolder(~, sessDat)
+            f = sprintf('V%i', sessDat.snumber);
+        end
+        function f = hdrinfoFolder(~, sessDat)
+            f = sprintf('V%i', sessDat.snumber);
+        end
+        function f = mriFolder(~, sessDat)
+            f = sprintf('V%i', sessDat.snumber);
+        end
+        function f = petFolder(~, sessDat)
+            f = sprintf('V%i', sessDat.snumber);
+        end      
+        
+        function fn = ho_fn(~, sessDat)
+            fn = sprintf('%sho%i.4dfp.nii.gz', sessDat.sessionFolder, sessDat.snumber);
+        end
+        function fn = oc_fn(~, sessDat)
+            fn = sprintf('%soc%i.4dfp.nii.gz', sessDat.sessionFolder, sessDat.snumber);
+        end
+        function fn = mpr_fn(~, sessDat)
+            fn = sprintf('%s_mpr.4dfp.nii.gz', sessDat.sessionFolder);
+        end
+        function fn = oo_fn(~, sessDat)
+            fn = sprintf('%soo%i.4dfp.nii.gz', sessDat.sessionFolder, sessDat.snumber);
+        end
+        function fn = petfov_fn(~)
+            fn = 'PETFOV.4dfp.nii.gz';
+        end
+        function fn = tof_fn(~)
+            fn = 'TOF_ART.4dfp.nii.gz';
+        end
+        function fn = toffov_fn(~)
+            fn = 'AIFFOV.4dfp.nii.gz';
+        end
+    end
+    
+    %% PROTECTED
+    
+	methods (Access = protected)   
  		function this = StudyDataSingleton(varargin)
  			this = this@mlpipeline.StudyDataSingleton(varargin{:});
+            
+            dt = mlsystem.DirTools(this.subjectsDir);
+            fqdns = {};
+            for di = 1:length(dt.dns)
+                if (strcmp(dt.dns{di}(1),   'p')  || ...
+                    strcmp(dt.dns{di}(1:2), 'NP') || ...
+                    strcmp(dt.dns{di}(1:2), 'TW') || ...
+                    strcmp(dt.dns{di}(1:5), 'HYGLY'))
+                    fqdns = [fqdns dt.fqdns(di)];
+                end
+            end
+            this.sessionDataComposite_ = ...
+                mlpatterns.CellComposite( ...
+                    cellfun(@(x) mlpipeline.SessionData('studyData', this, 'sessionPath', x), ...
+                    fqdns, 'UniformOutput', false));            
+            this.registerThis;
+        end
+        function registerThis(this)
             mlpipeline.StudyDataSingletons.register('raichle', this);
- 		end
+        end
  	end 
 
 	%  Created with Newcl by John J. Lee after newfcn by Frank Gonzalez-Morphy
---
+++ src/+mlraichle/TestDataSingleton.m
@@ -0,0 +1,68 @@
+classdef TestDataSingleton < mlraichle.StudyDataSingleton
+	%% TESTDATASINGLETON  
+
+	%  $Revision$
+ 	%  was created 30-Jan-2016 18:11:15
+ 	%  by jjlee,
+ 	%  last modified $LastChangedDate$
+ 	%  and checked into repository /Users/jjlee/Local/src/mlcvl/mlraichle/src/+mlraichle.
+ 	%% It was developed on Matlab 9.0.0.307022 (R2016a) Prerelease for MACI64.
+ 	
+
+    methods (Static)
+        function this = instance(qualifier)
+            persistent instance_            
+            if (exist('qualifier','var'))
+                assert(ischar(qualifier));
+                if (strcmp(qualifier, 'initialize'))
+                    instance_ = [];
+                end
+            end            
+            if (isempty(instance_))
+                instance_ = mlraichle.TestDataSingleton();
+            end
+            this = instance_;
+        end
+        function        register(varargin)
+            %% REGISTER
+            %  @param []:  if this class' persistent instance
+            %  has not been registered, it will be registered via instance() call to the ctor; if it
+            %  has already been registered, it will not be re-registered.
+            %  @param ['initialize']:  any registrations made by the ctor will be repeated.
+            
+            mlraichle.TestDataSingleton.instance(varargin{:});
+        end
+    end  
+
+    %% PROTECTED
+    
+	methods (Access = protected)
+ 		function this = TestDataSingleton(varargin)
+ 			this = this@mlraichle.StudyDataSingleton(varargin{:});
+            
+            this.raichleTrunk = '/Volumes/InnominateHD3/Local/test/raichle/PPGdata';
+            
+            dt = mlsystem.DirTools(this.subjectsDir);
+            fqdns = {};
+            for di = 1:length(dt.dns)
+                if (strcmp(dt.dns{di}(1),   'p')  || ...
+                    strcmp(dt.dns{di}(1:2), 'NP') || ...
+                    strcmp(dt.dns{di}(1:2), 'TW') || ...
+                    strcmp(dt.dns{di}(1:5), 'HYGLY'))
+                    fqdns = [fqdns dt.fqdns(di)];
+                end
+            end
+            this.sessionDataComposite_ = ...
+                mlpatterns.CellComposite( ...
+                    cellfun(@(x) mlpipeline.SessionData('studyData', this, 'sessionPath', x), ...
+                    fqdns, 'UniformOutput', false));                
+            this.registerThis;
+ 		end
+        function registerThis(this)
+            mlpipeline.StudyDataSingletons.register('test_raichle', this);
+        end
+ 	end 
+
+	%  Created with Newcl by John J. Lee after newfcn by Frank Gonzalez-Morphy
+ end
+
