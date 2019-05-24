d40bddb540bd0ecb20a947e5d0d4a6f7017d7881
John J. Lee <16329959+jjleewustledu@users.noreply.github.com>
2019-05-24 15:31:47 -0500

    extract superclass mlnipet.ProjectData

--- src/+mlraichle/ProjectData.m
+++ src/+mlraichle/ProjectData.m
@@ -1,4 +1,4 @@
-classdef ProjectData < mlpipeline.ProjectData
+classdef ProjectData < mlnipet.ProjectData
 	%% PROJECTDATA  
 
 	%  $Revision$
@@ -6,25 +6,9 @@ classdef ProjectData < mlpipeline.ProjectData
  	%  last modified $LastChangedDate$ and placed into repository /Users/jjlee/MATLAB-Drive/mlraichle/src/+mlraichle.
  	%% It was developed on Matlab 9.5.0.1067069 (R2018b) Update 4 for MACI64.  Copyright 2019 John Joowon Lee.
  	
-	properties (Dependent)
-        jsonDir
- 	end
-
 	methods 
-        
-        %% GET
-        
-        function g = get.jsonDir(~)
-            g = getenv('SUBJECTS_DIR');
-        end
-        
-        %%
-        
         function g = getProjectFolder(this, s)
             g = this.session2projectMap_(s);
-        end
-        function g = getProjectPath(this, s)
-            g = fullfile(this.projectsDir, this.getProjectFolder(s), '');
         end
         function p = session2project(this, s)
             %% e.g.:  {'CNDA_E1234','ses-E1234'} -> 'CCIR_00123'
@@ -40,7 +24,7 @@ classdef ProjectData < mlpipeline.ProjectData
  			%% PROJECTDATA
  			%  @param .
 
- 			this = this@mlpipeline.ProjectData(varargin{:});
+ 			this = this@mlnipet.ProjectData(varargin{:});
             this = this.aufbauMap;
  		end
     end 
