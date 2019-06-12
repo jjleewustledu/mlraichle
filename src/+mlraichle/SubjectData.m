22ab0e935a26074f6c12e67a976574af34e29563
John J. Lee <16329959+jjleewustledu@users.noreply.github.com>
2019-06-12 18:39:06 -0500

    move aufbauSubject*

--- src/+mlraichle/SubjectData.m
+++ src/+mlraichle/SubjectData.m
@@ -12,48 +12,6 @@ classdef SubjectData < mlnipet.SubjectData
     end
 
 	methods 
-        
-        %%
-        
-        function        aufbauSubjectsDir(this)
-            %% e. g., /subjectsDir/{sub-S123456, sub-S123457, ...}
-            
-            S = this.subjectsStruct_;
-            for sub = fields(S)'
-                d = this.ensuredirSub(S.(sub{1}).sid);
-                this.aufbauSubjectPath(d, S.(sub{1}));
-            end
-        end
-        function        aufbauSubjectPath(this, sub_pth, S_sub)
-            %% e. g., /subjectsDir/sub-S40037/{ses-E182819, ses-E182853, ...}/tracer.4dfp.*, with sym-linked tracer.4dfp.*
-            
-            if isfield(S_sub, 'aliases')
-                for asub = fields(S_sub.aliases)'
-                    this.aufbauSubjectPath(sub_pth, S_sub.aliases.(asub{1}));
-                end
-            end
-            
-            % base case
-            assert(isfield(S_sub, 'experiments'))
-            for e = S_sub.experiments'
-                d = this.ensuredirSes(sub_pth, e{1});
-                fcell = this.ensuredirsScans(d);
-                if (~isempty(fcell))
-                    e1 = this.experimentID_to_ses(e{1});
-                    try
-                        this.lns_tracers( ...
-                            fullfile(this.projectData_.getProjectPath(e1), e1, ''), ...
-                            fullfile(sub_pth, e1, ''), ...
-                            fcell);
-                        this.lns_surfer( ...
-                            fullfile(this.projectData_.getProjectPath(e1), e1, ''), ...
-                            fullfile(sub_pth, e1, ''));
-                    catch ME
-                        handwarning(ME);
-                    end
-                end
-            end
-        end
         function sub  = subjectID_to_sub(~, sid)
             %% abbreviates sub-CNDA01_S12345 -> sub-S12345
             
