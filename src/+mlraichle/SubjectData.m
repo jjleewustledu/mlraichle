e4d6e925a9e0af7a38d16838180975aa20a43310
John J. Lee <16329959+jjleewustledu@users.noreply.github.com>
2019-06-20 17:32:26 -0500

    bugfix for construct_subject

--- src/+mlraichle/SubjectData.m
+++ src/+mlraichle/SubjectData.m
@@ -10,8 +10,56 @@ classdef SubjectData < mlnipet.SubjectData
         TRACERS = {'FDG' 'OC' 'OO' 'HO'}
         EXTS = {'.4dfp.hdr' '.4dfp.ifh' '.4dfp.img' '.4dfp.img.rec'};
     end
+    
+    methods (Static)
+        function obj = createProjectData(varargin)
+            obj = mlraichle.ProjectData(varargin{:});
+        end
+    end
 
 	methods 
+        function sesf = subFolder2sesFolder(this, subf)
+            %% requires well-defined cell-array this.subjectsJson.
+            %  @param subf is a subject folder.
+            %  @returns first-found non-trivial session folder in the subject folder.
+            
+            json = this.subjectsJson;
+            subs = fields(json);
+            substr = split(subf, '-');
+            substr = substr{2};
+            sesf = '';
+            for sub = ensureRowVector(subs)
+                jsonsub = json.(sub{1});
+                if lstrfind(jsonsub.id, substr)
+                    sesf = searchExperiments(jsonsub);
+                    if isempty(sesf) && isfield(jsonsub, 'aliases')
+                        for alias = ensureRowVector(fields(jsonsub.aliases))
+                            jsonalias = jsonsub.aliases.(alias{1});
+                            sesf = searchExperiments(jsonalias);
+                        end
+                    end
+                end
+            end   
+            
+            function sesf = searchExperiments(sub)
+                sesf = '';
+                if isfield(sub, 'experiments')
+                    for e = ensureRowVector(sub.experiments)
+                        sesstr = split(e{1}, '_');
+                        sesstr = sesstr{2};
+                        if foundScanIn(['ses-' sesstr])
+                            sesf = ['ses-' sesstr];
+                            return
+                        end
+                    end
+                end
+            end
+                
+            function tf = foundScanIn(sesf)
+                dt = mlsystem.DirTool(fullfile(this.subjectPath, sesf, '*_DT*.000000-Converted-AC'));
+                tf = ~isempty(dt.fqdns);
+            end
+        end
         function sub  = subjectID_to_sub(~, sid)
             %% abbreviates sub-CNDA01_S12345 -> sub-S12345
             
