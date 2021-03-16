47423c762bc6517064c99bdb0720e11a0a75a00f
John J. Lee <16329959+jjleewustledu@users.noreply.github.com>
2021-03-16 14:33:10 -0500

    refactored mlnipet.MetabolicSessionData

--- src/+mlraichle/SessionData.m
+++ src/+mlraichle/SessionData.m
@@ -1,4 +1,4 @@
-classdef SessionData < mlnipet.ResolvingSessionData
+classdef SessionData < mlnipet.MetabolicSessionData
 	%% SESSIONDATA  
 
 	%  $Revision$
@@ -96,13 +96,9 @@ classdef SessionData < mlnipet.ResolvingSessionData
     end
 
     methods
-        function obj  = aifsOnAtlas(this, varargin)
-            tr = lower(this.tracer);
-            obj = this.metricOnAtlas(['aif_' tr], varargin{:});
-        end
-        function obj  = brainOnAtlas(this, varargin)
-            obj = this.metricOnAtlas('brain', 'datetime', '',varargin{:});
-        end
+            
+        %% 
+        
         function g    = getStudyCensus(this)
             g = mlraichle.StudyCensus(this.STUDY_CENSUS_XLSX_FN', 'sessionData', this);
         end 
@@ -113,53 +109,6 @@ classdef SessionData < mlnipet.ResolvingSessionData
             end
             this.subjectData_.studyData = studyd;
         end
-        function obj  = metricOnAtlas(this, metric, varargin)
-            %% METRICONATLAS appends fileprefixes with information from this.dataAugmentation
-            %  @param required metric is char.
-            %  @param datetime is datetime or char, .e.g., '20200101000000' | ''.
-            %  @param dateonlhy is logical.
-            %  @param tags is char, e.g., '_b43_wmparc1_b43'
-            
-            ip = inputParser;
-            ip.KeepUnmatched = true;
-            addRequired(ip, 'metric', @ischar)
-            addParameter(ip, 'datetime', this.datetime, @(x) isdatetime(x) || ischar(x))
-            addParameter(ip, 'dateonly', false, @islogical)
-            addParameter(ip, 'tags', '', @ischar)
-            parse(ip, metric, varargin{:})
-            ipr = ip.Results;
-            
-            if ischar(ipr.datetime)
-                adatestr = ipr.datetime;
-            end
-            if isdatetime(ipr.datetime)
-                if ipr.dateonly
-                    adatestr = ['dt' datestr(ipr.datetime, 'yyyymmdd') '000000'];
-                else
-                    adatestr = ['dt' datestr(ipr.datetime, 'yyyymmddHHMMSS')];
-                end
-            end
-            
-            fqfn = fullfile( ...
-                this.dataPath, ...
-                sprintf('%s%s%s%s%s', ...
-                        lower(ipr.metric), ...
-                        adatestr, ...
-                        this.registry.atlasTag, ...
-                        ipr.tags, ...
-                        this.filetypeExt));
-            obj  = this.fqfilenameObject(fqfn, varargin{:});
-        end
-        function obj  = mprForReconall(this, varargin)
-            obj = this.fqfilenameObject( ...
-                fullfile(this.sessionPath, ['mpr' this.filetypeExt]), varargin{:});            
-        end        
-        function obj  = parcOnAtlas(this, varargin)
-            fqfn = fullfile( ...
-                this.dataPath, ...
-                sprintf('%s%s%s', this.parcellation, this.registry.atlasTag, this.filetypeExt));
-            obj  = this.fqfilenameObject(fqfn, varargin{:});
-        end
         function loc  = tracerRawdataLocation(this, varargin)
             %% Siemens legacy
             ipr = this.iprLocation(varargin{:});
@@ -169,106 +118,9 @@ classdef SessionData < mlnipet.ResolvingSessionData
             end
             loc = this.tracerConvertedLocation(varargin{:});
         end        
-        function obj  = tracerOnAtlas(this, varargin)
-            obj = this.metricOnAtlas(this.tracer, varargin{:});
-        end
-        function obj  = venousOnAtlas(this, varargin)
-            obj = this.metricOnAtlas('venous', 'datetime', '',varargin{:});
-        end
-        function obj  = wbrain1OnAtlas(this, varargin)
-            
-            % prepare as needed
-            wm_fqfn = this.wmparc1OnAtlas('typ', 'fqfilename');
-            assert(isfile(wm_fqfn))
-            wb_fqfn = strrep(wm_fqfn, 'wmparc1', 'wbrain1');
-            if ~isfile(wb_fqfn)
-                ic = mlfourd.ImagingContext2(wm_fqfn);
-                ic = ic.binarized();
-                ic.fileprefix = strrep(ic.fileprefix, 'wmparc1', 'wbrain1');
-                ic.save()
-            end
-            
-            obj = this.metricOnAtlas('wbrain1', 'datetime', '',varargin{:});
-        end
-        function obj  = wmparcOnAtlas(this, varargin)
-            obj = this.metricOnAtlas('wmparc', 'datetime', '',varargin{:});
-        end
-        function obj  = wmparc1OnAtlas(this, varargin)
-            obj = this.metricOnAtlas('wmparc1', 'datetime', '',varargin{:});
-        end
-        
-        %% Metabolism
-        
-        function obj  = agiOnAtlas(this, varargin)
-            % dag := cmrglc - cmro2/6 \approx aerobic glycolysis
-            
-            obj = this.metricOnAtlas('agi', varargin{:});
-        end        
-        function obj  = cbfOnAtlas(this, varargin)
-            obj = this.metricOnAtlas('cbf', varargin{:});
-        end
-        function obj  = cbvOnAtlas(this, varargin)
-            obj = this.metricOnAtlas('cbv', varargin{:});
-        end
-        function obj  = chiOnAtlas(this, varargin)
-            obj = this.metricOnAtlas('chi', varargin{:});
-        end
-        function obj  = cmrglcOnAtlas(this, varargin)
-            obj = this.metricOnAtlas('cmrglc', varargin{:});
-        end
-        function obj  = cmro2OnAtlas(this, varargin)
-            obj = this.metricOnAtlas('cmro', varargin{:});
-        end
-        function obj  = coOnAtlas(this, varargin)
-            obj = this.metricOnAtlas('co', varargin{:});
-        end
-        function obj  = fdgOnAtlas(this, varargin)            
-            obj = this.metricOnAtlas('fdg', varargin{:});
-        end
-        function obj  = fsOnAtlas(this, varargin)
-            obj = this.metricOnAtlas('fs', varargin{:});
-        end
-        function obj  = gsOnAtlas(this, varargin)
-            obj = this.metricOnAtlas('gs', varargin{:});
-        end
-        function obj  = hoOnAtlas(this, varargin)
-            obj = this.metricOnAtlas('ho', varargin{:});
-        end
-        function obj  = KsOnAtlas(this, varargin)
-            obj = this.metricOnAtlas('Ks', varargin{:});
-        end
-        function obj  = ksOnAtlas(this, varargin)
-            obj = this.metricOnAtlas('ks', varargin{:});
-        end
-        function obj  = maskOnAtlas(this, varargin)
-            obj = this.metricOnAtlas('mask', varargin{:});
-        end
-        function obj  = ocOnAtlas(this, varargin)
-            obj = this.metricOnAtlas('oc', varargin{:});
-        end
-        function obj  = ooOnAtlas(this, varargin)
-            obj = this.metricOnAtlas('oo', varargin{:});
-        end
-        function obj  = osOnAtlas(this, varargin)
-            obj = this.metricOnAtlas('os', varargin{:});
-        end
-        function obj  = oefOnAtlas(this, varargin)
-            obj = this.metricOnAtlas('oef', varargin{:});
-        end
-        function obj  = ogiOnAtlas(this, varargin)
-            obj = this.metricOnAtlas('ogi', varargin{:});
-        end
-        function obj  = v1OnAtlas(this, varargin)
-            obj = this.metricOnAtlas('v1', varargin{:});
-        end
-        function obj  = vsOnAtlas(this, varargin)
-            obj = this.metricOnAtlas('vs', varargin{:});
-        end
-                
-        %% 
         
       	function this = SessionData(varargin)
- 			this = this@mlnipet.ResolvingSessionData(varargin{:});
+ 			this = this@mlnipet.MetabolicSessionData(varargin{:});
             if isempty(this.studyData_)
                 this.studyData_ = mlraichle.StudyData();
             end
